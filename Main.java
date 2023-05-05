b72eb9706b8552e5d492819964c366bb43409db5
laces86 <laces86@gmail.com>
2023-05-05 20:10:41 +0200

    Create Main.java
    Main app

---
+++ Main.java
@@ -0,0 +1,14 @@
+package jt;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Main();
+    }
+    
+    public Main() {
+        System.out.println("alma");
+    }
+
+
+}
